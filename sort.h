984ae98640c0117aa624a318e28de6dfcd38d8e9
Medo20300 <medhatconcrete@gmail.com>
2024-01-20 15:50:24 -0800

    Med

--- sort.h
+++ sort.h
@@ -32,5 +32,5 @@ void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void swappingAdjacentNode(listint_t **list, listint_t *firstNode, listint_t *secondNode);
-
+void counting_sort(int *array, size_t size);
 #endif
