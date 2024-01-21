e4123e9f5e68f3b4fcf5a2c0e713101f6015da17
FOXAYA <omniasaged111@gmail.com>
2024-01-21 04:47:40 -0800

    updated

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -9,40 +9,40 @@
  */
 void counting_sort(int *array, size_t size)
 {
-int current_element, total_count, maximum_value;
-int *element_count, *sorted_array;
+	int current_element, total_count, maximum_value;
+	int *element_count, *sorted_array;
 
-if (!array || size < 2)
-return;
-maximum_value = array[0];
-for (current_element = 0; current_element < (int)size; current_element++)
-{
-if (array[current_element] > maximum_value)
-maximum_value = array[current_element];
-}
-element_count = calloc((maximum_value + 1), sizeof(int));
-if (!element_count)
-return;
-for (current_element = 0; current_element < (int)size; current_element++)
-element_count[array[current_element]]++;
-for (current_element = 0, total_count = 0; current_element <
-maximum_value + 1; current_element++)
-{
-total_count = element_count[current_element] + total_count;
-element_count[current_element] = total_count;
-}
-print_array(element_count, maximum_value + 1);
-sorted_array = malloc(sizeof(int) * size);
-if (!sorted_array)
-return;
-for (current_element = 0; current_element < (int)size; current_element++)
-{
-sorted_array[element_count[array[current_element]] - 1]
-= array[current_element];
-element_count[array[current_element]]--;
-}
-for (current_element = 0; current_element < (int)size; current_element++)
-array[current_element] = sorted_array[current_element];
-free(element_count);
-free(sorted_array);
+	if (!array || size < 2)
+		return;
+	maximum_value = array[0];
+	for (current_element = 0; current_element < (int)size; current_element++)
+	{
+		if (array[current_element] > maximum_value)
+			maximum_value = array[current_element];
+	}
+	element_count = calloc((maximum_value + 1), sizeof(int));
+	if (!element_count)
+		return;
+	for (current_element = 0; current_element < (int)size; current_element++)
+		element_count[array[current_element]]++;
+	for (current_element = 0, total_count = 0; current_element <
+			maximum_value + 1; current_element++)
+	{
+		total_count = element_count[current_element] + total_count;
+		element_count[current_element] = total_count;
+	}
+	print_array(element_count, maximum_value + 1);
+	sorted_array = malloc(sizeof(int) * size);
+	if (!sorted_array)
+		return;
+	for (current_element = 0; current_element < (int)size; current_element++)
+	{
+		sorted_array[element_count[array[current_element]] - 1]
+			= array[current_element];
+		element_count[array[current_element]]--;
+	}
+	for (current_element = 0; current_element < (int)size; current_element++)
+		array[current_element] = sorted_array[current_element];
+	free(element_count);
+	free(sorted_array);
 }
